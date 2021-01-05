90e90a0ee8f1cd1208757b28645965010aeab7ed
Federico Matarrita Arata <federico@usematics.com>
2021-01-05 11:45:12 -0600

    add FindNamedMatches

--- helpers.go
+++ helpers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
 
 	"github.com/joho/godotenv"
 )
@@ -21,4 +22,15 @@ func GetConfig() map[string]string {
 	env, err := godotenv.Read()
 	HandleError(err)
 	return env
+}
+
+//FindNamedMatches returns a map with the named matches for a regexp
+func FindNamedMatches(regex *regexp.Regexp, str string) map[string]string {
+	match := regex.FindStringSubmatch(str)
+
+	results := map[string]string{}
+	for i, name := range match {
+		results[regex.SubexpNames()[i]] = name
+	}
+	return results
 }
