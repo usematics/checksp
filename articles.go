c38b76de0b0f60d305041e3958b14d2093d0d316
Federico Matarrita Arata <federico@usematics.com>
2021-01-15 15:08:49 -0600

    Add author, refactor csv log

--- articles.go
+++ articles.go
@@ -14,9 +14,10 @@ import (
 
 //Article holds text and title for the artilces with a species map.
 type Article struct {
-	ID    int
-	Title string
-	Text  string
+	ID     int
+	Title  string
+	Text   string
+	Author string
 }
 
 //Articles checks that all species home pages articles have a map an that the map has corresponding records on the inventory API
@@ -25,7 +26,7 @@ func Articles() {
 	db, err := sql.Open("mysql", env["USER"]+":"+env["PASSWORD"]+"@"+"/"+env["DATABASE"])
 	HandleError(err)
 	defer db.Close()
-	query, err := db.Prepare("select ac.id, ac.title, ac.introtext from acgj_categories cat, acgj_content as ac where cat.parent_id = 279 and cat.id = ac.catid and ac.state = 1")
+	query, err := db.Prepare("select ac.id, ac.title, ac.introtext, u.name from acgj_categories cat, acgj_content as ac, acgj_users as u where cat.parent_id = 279 and cat.id = ac.catid and ac.state = 1 and ac.created_by = u.id")
 
 	HandleError(err)
 	rows, err := query.Query()
@@ -41,13 +42,14 @@ func Articles() {
 	errorFlag := true
 	for rows.Next() {
 		var article Article
-		err = rows.Scan(&article.ID, &article.Title, &article.Text)
+		err = rows.Scan(&article.ID, &article.Title, &article.Text, &article.Author)
 		if err != nil {
 			continue
 		}
 		errorFlag = false //could all be errors?
 		re := regexp.MustCompile(`{(\s)*mapasp(\s|\w|,)*}`)
 		matched := re.Find([]byte(article.Text))
+		var message string
 		//Do we have a map plugin in the article?
 		if matched != nil {
 			re = regexp.MustCompile(`{(\s)*mapasp(\s)*`)
@@ -55,14 +57,11 @@ func Articles() {
 			re = regexp.MustCompile(`(\s)*}`)
 			mapSpecies = re.ReplaceAll(mapSpecies, []byte(""))
 			species := string(mapSpecies)
+			article.Title = species
 			//Do we have info for that species on the API?
 			hasInfo := GetSpPoints(species)
 			if !hasInfo {
-				missing := make([]string, 2)
-				missing[0] = species
-				missing[1] = string("Mapa Sin Info en API")
-				_ = csvWriter.Write(missing)
-				fmt.Println(species + " Mapa sin Info en API")
+				message = "Mapa Sin Info en API"
 			}
 
 		} else {
@@ -70,35 +69,32 @@ func Articles() {
 			match := FindNamedMatches(re, article.Title)
 			species := fmt.Sprintf("%s %s", s.TrimSpace(match["sp1"]), s.TrimSpace(match["sp2"]))
 			species = s.TrimSpace(species)
+
 			//Can we get a species name from the title of the article?
 			if species != "" {
+				article.Title = species
 				hasInfo := GetSpPoints(species)
 				if !hasInfo {
-					missing := make([]string, 2)
-					missing[0] = species
-					missing[1] = string("Sin Mapa y sin info en API")
-					_ = csvWriter.Write(missing)
-					fmt.Println(species + " Sin Mapa y sin info en API")
+					message = "Sin Mapa y sin info en API"
 				} else {
 					//yes,  append map to species page
 					article.Text += "{mapasp " + species + "}"
 					_, err := db.Query("Update acgj_content set introtext = ? where id = ?", article.Text, article.ID)
 					if err != nil {
-						fmt.Println(err)
+						message = err.Error()
 					} else {
-						fmt.Println("FIXED: " + species)
+						message = "Arreglada"
 					}
 
 				}
 
 			} else {
-				missing := make([]string, 2)
-				missing[0] = article.Title
-				missing[1] = "Título no calza en especie"
-				_ = csvWriter.Write(missing)
-				fmt.Println(missing[0] + " Título no calza en especie")
+				message = "Título no calza en especie"
 			}
 
+		}
+		if message != "" {
+			logInCsv(article, message, csvWriter)
 		}
 	}
 	csvWriter.Flush()
@@ -107,4 +103,13 @@ func Articles() {
 	} else {
 		fmt.Println("No valid articles to verify")
 	}
+}
+
+func logInCsv(article Article, message string, csvWriter *csv.Writer) {
+	missing := make([]string, 3)
+	missing[0] = article.Author
+	missing[1] = article.Title
+	missing[2] = message
+	_ = csvWriter.Write(missing)
+	fmt.Println(missing[1] + " " + message)
 }
